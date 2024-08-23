94cb66e4efa03b3151491825ed229fe27987e2fa
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-08-23 14:54:26 +0530

    Key Animation

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    public float YPosition;
+    public Animator animator;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>() != null)
         {
             PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
             playerController.PickUpKey();
+            animator.SetBool("Dead", true);
+        }
+    }
+
+    private void Update()
+    {
+        Vector2 position = transform.position;
+
+        if (position.y > YPosition)
+        {
+            Debug.Log("hi");
             Destroy(gameObject);
         }
     }
